8de8814d824810ca83b50c6041fb5189762786e9
Jeff Brower <jeffbrower0@gmail.com>
2025-03-11 12:21:17 -0400

    add test files via github ui

---
+++ FormatterTest.java
@@ -0,0 +1,58 @@
+package com.jeffbrower;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PushbackReader;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class FormatterTest {
+   @ParameterizedTest
+   @ValueSource(strings = {
+      "test1",
+      "test2",
+      "test3",
+      "test4",
+      "test5",
+      "test6",
+   })
+   void test(final String baseName) throws IOException {
+      final String in, out;
+      try (
+         final InputStream isIn = ClassLoader.getSystemResourceAsStream(baseName + ".in.xml");
+         final InputStream isOut = ClassLoader.getSystemResourceAsStream(baseName + ".out.xml")
+      ) {
+         in = new String(isIn.readAllBytes(), StandardCharsets.UTF_8);
+         out = new String(isOut.readAllBytes(), StandardCharsets.UTF_8);
+      }
+
+      final FormatOptions o = new FormatOptions();
+      o.useTabs = true;
+      o.endOfLine = LineEnding.SYSTEM;
+
+      try (
+         final StringWriter w = new StringWriter();
+         final Formatter f = new Formatter(new PushbackReader(new StringReader(in)), w, o)
+      ) {
+         while (f.parseAny()) {
+            // keep going
+
+            if (Formatter.DEBUG) {
+               System.out.println("=".repeat(100));
+               System.out.println(w.toString() + "|");
+               System.out.println("=".repeat(100));
+               f.printDebugInfo();
+               System.out.println("=".repeat(100));
+            }
+         }
+
+         assertEquals(out, w.toString());
+      }
+   }
+}
